cbc804d1fe74d291efd4cb427896b145df698719
Shushant Rishav <54536673+Rishu20@users.noreply.github.com>
2023-02-08 20:28:00 +0530

    update

--- Statics/script.js
+++ Statics/script.js
@@ -20,6 +20,7 @@ window.onscroll = () => {
     : navbar.classList.remove("sticky")/banner.classList.remove("hidden");
 };
 
+$(document ).ready(function(){
 const elements = document.getElementsByClassName("alpha");
 
 for (let i = 0; i <= elements.length; i++) {
@@ -31,8 +32,39 @@ for (let i = 0; i <= elements.length; i++) {
     elements[i].classList.add("animated");
   });
 }
-
+});
 function toggleMute() {
   var myAudio = document.getElementById('audio_playo24');
   myAudio.muted = !myAudio.muted;
+}
+
+
+const btn = document.getElementById('music');
+
+btn.addEventListener('click', function handleClick() {
+  const initialText = ' Unmute Audio';
+  btn.className= "fa-solid fa-volume-high";
+
+  if (btn.textContent.toLowerCase().includes(initialText.toLowerCase())) {
+    btn.textContent = ' Mute Audio';
+    btn.className= "fa-solid fa-volume-xmark";
+  } else {
+    btn.textContent = initialText;
+  }
+});
+
+
+
+function SubForm (){
+  $.ajax({
+    url:"https://api.apispreadsheets.com/data/J052l0u8EtGWGmns/",
+    type:"POST",
+    data:$("#myForm").serializeArray(),
+    success: function(){
+      alert("Form Data Submitted :)")
+    },
+    error: function(){
+      alert("There was an error :(")
+    }
+  });
 }
