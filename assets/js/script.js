7505d35057576f88857d521d4fcd8f1be272dad8
Shushant Rishav <54536673+Rishu20@users.noreply.github.com>
2023-12-28 05:27:24 +0530

    update

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,71 +1,53 @@
+// Create a function to handle the intersection callback
+function handleIntersection(entries, observer) {
+  entries.forEach((entry) => {
+    const sectionId = entry.target.getAttribute("id");
+    const navLink = document.querySelector(
+      `.nav-links li a[href="#${sectionId}"]`
+    );
+    if (navLink !== null) {
+      if (entry.isIntersecting) {
+        // Add the "active" class to the corresponding nav link
+        navLink.classList.add("active");
+      } else {
+        // Remove the "active" class from the nav link
+        navLink.classList.remove("active");
+      }
+    }
+  });
+}
+
+// Create the Intersection Observer instance
+const observer = new IntersectionObserver(handleIntersection, {
+  root: null, // Use the viewport as the root element
+  rootMargin: "-50% 0px", // Trigger when the top of the section is near the top of the display
+  threshold: 0, // Trigger when the section is fully visible
+});
+
+
+// Observe each section
+document.querySelectorAll("section").forEach((section) => {
+  observer.observe(section);
+});
+
 const banner = document.querySelector(".canvas");
+const navbar = document.querySelector(".navbar ");
 window.onscroll = () => {
-  this.scrollY > 20
-    ? banner.classList.add("hidden")
-    : banner.classList.remove("hidden");
+  this.scrollY > 10
+    ? banner.classList.add("hidden") | navbar.classList.add("sticky")
+    : banner.classList.remove("hidden") | navbar.classList.remove("sticky");
 };
-
-//nav bar toggle
-$(document).ready(function () {
-  $(".toggle").click(function () {
-    $(".toggle").toggleClass("active");
-    $("nav").toggleClass("active");
-  });
-});
-function toggle_close() {
-  $(".toggle").toggleClass("active");
-  $("nav").toggleClass("active");
-}
-// change theme
-const btn = document.querySelector(".btn-toggle");
-const theme = document.querySelector("#theme-link");
-btn.addEventListener("click", function () {
-  // Swap out the URL for the different stylesheets
-  if (theme.getAttribute("href") == "./assets/css/style.css") {
-    theme.href = "./assets/css/style_dark.css";
-  } else {
-    theme.href = "./assets/css/style.css";
-  }
-});
-// change icon on click
-$(document).ready(() => {
-  const a = $(".btn");
-
-  a.click(() => {
-    if (a.text() == " ") {
-      a.html('<i class="fas fa-sun"></i>');
-    } else {
-      a.html('<i class="fas fa-moon"></i> ');
-    }
-  });
-});
-
-function toggleMute() {
-  var myAudio = document.getElementById("audio_playo24");
-  myAudio.muted = !myAudio.muted;
-
-  var audioIcon = document.getElementById("audio_icon");
-  if (myAudio.muted) {
-    audioIcon.classList.remove("fa-volume-up");
-    audioIcon.classList.add("fa-volume-mute");
-  } else {
-    audioIcon.classList.remove("fa-volume-mute");
-    audioIcon.classList.add("fa-volume-up");
-  }
-}
-
-
-function SubForm (){
+function SubForm() {
   $.ajax({
-    url:"https://api.apispreadsheets.com/data/TI9POZvvjpBfQW1t/",
-    type:"post",
-    data:$("#myForm").serializeArray(),
-    success: function(){
-      alert("Form Data Submitted")
-      $("#myForm")[0].reset()
+    url: "https://api.apispreadsheets.com/data/TI9POZvvjpBfQW1t/",
+    type: "post",
+    data: $("#myForm").serializeArray(),
+    success: function () {
+      alert("Form Data Submitted");
+      $("#myForm")[0].reset();
     },
-    error: function(){
-      alert("There was an error :(")
-    }
+    error: function () {
+      alert("There was an error :(");
+    },
   });
 }
